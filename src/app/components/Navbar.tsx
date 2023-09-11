590703b03c77e9d0410241c32ac4ee8cfd93df0c
Shihab0 <ataharshihab1@gmail.com>
2023-09-11 16:51:14 +0600

    responsive navbar for pc  added

--- src/app/components/Navbar.tsx
+++ src/app/components/Navbar.tsx
@@ -1,22 +1,44 @@
 import React from "react";
 import Menu from "./Menu";
 import Link from "next/link";
+import Cart from "./Cart";
+import Image from "next/image";
 
 const Navbar = () => {
+  const user = false;
   return (
-    <div className="flex justify-between items-center uppercase border-b-2 border-b-red-500 h-16 text-red-500 p-4">
+    <div className="flex font-semibold justify-between items-center uppercase border-b-2 border-b-red-500 h-16 text-red-500 p-4 lg:px-16 md:px-10 ">
+      {/* Left links */}
+      <div className="hidden md:flex gap-4 justify-start flex-1">
+        <Link href="/">Homepage</Link>
+        <Link href="/menu">Menu</Link>
+        <Link href="/contact">Contact</Link>
+      </div>
       {/* Logo */}
-      <div>
+      <div className="flex-1 text-center">
         <Link href={"/"}>
           {" "}
-          <span className="font-semibold md:font-bold text-xl">
+          <span className="font-semibold md:font-bold text-xl ">
             MakShop
           </span>{" "}
         </Link>
       </div>
       {/* Mobile menu */}
-      <div>
+      <div className="md:hidden">
         <Menu></Menu>
+      </div>
+      {/* Right links */}
+      <div className="hidden md:flex gap-4 flex-1 justify-end">
+        <div className="flex gap-1 bg-orange-300 rounded-md cursor-pointer px-2 items-center lg:static md:absolute top-2 right-2">
+          <Image width={20} height={20} src="/phone.png" alt="" />
+          <span>3922</span>
+        </div>
+        {!user ? (
+          <Link href="/login">Login</Link>
+        ) : (
+          <Link href="/orders">Orders</Link>
+        )}
+        <Cart></Cart>
       </div>
     </div>
   );
