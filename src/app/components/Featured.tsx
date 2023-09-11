0f8ce913056fd2ba10d9b8cb053a6b0fdaae8f1d
Shihab0 <ataharshihab1@gmail.com>
2023-09-12 01:58:12 +0600

    featured section added and resposivness done

--- src/app/components/Featured.tsx
+++ src/app/components/Featured.tsx
@@ -4,18 +4,18 @@ import React from "react";
 
 const Featured = () => {
   return (
-    <div className="w-screen overflow-x-scroll text-red-500 mt-6">
+    <div className="w-screen overflow-x-scroll  text-red-500 mt-6">
       {/* Wrapper */}
-      <div className="w-max flex">
+      <div className="w-max flex p-2">
         {/* single item */}
         {featuredProducts.map((product) => (
           <div
             key={product.id}
-            className="w-screen h-[60vh] flex flex-col items-center justify-around"
+            className="w-screen h-[60vh] flex flex-col items-center justify-around md:w-[50vw] lg:w-[33vw] lg:h-[70vh] hover:bg-fuchsia-50 "
           >
             {/* img */}
             {product.img && (
-              <div className="relative flex-1 w-full">
+              <div className="relative flex-1 w-full hover:scale-110 transition-all duration-500 ">
                 <Image
                   className="object-contain"
                   src={product.img}
@@ -25,7 +25,7 @@ const Featured = () => {
               </div>
             )}
             {/* text */}
-            <div className="flex-1 flex flex-col items-center gap-2 mt-2 p-2 text-justify">
+            <div className="flex-1 flex flex-col items-center gap-2 mt-2 p-3 text-justify ">
               <h1 className="font-bold text-xl uppercase">{product.title}</h1>
               <p className="text-center"> {product.desc} </p>
               <span className="font-semibold text-xl"> ${product.price} </span>
--- src/app/components/Notification.tsx
+++ src/app/components/Notification.tsx
@@ -3,7 +3,7 @@ import React from "react";
 const Notification = () => {
   return (
     <div className="h-12 bg-red-500 text-white text-center px-4 text-sm md:text-base flex items-center justify-center cursor-pointer">
-      Order upto 600 TK for free delivery! Order Now!
+      Order over 600 TK for free delivery! Order Now!
     </div>
   );
 };
