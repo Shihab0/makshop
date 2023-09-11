12172f57338271cb67aba90953160ccf7d16b688
Shihab0 <ataharshihab1@gmail.com>
2023-09-12 00:41:29 +0600

    feature section added for mobile

--- src/app/components/Featured.tsx
+++ src/app/components/Featured.tsx
@@ -1,3 +1,4 @@
+import { featuredProducts } from "@/data";
 import Image from "next/image";
 import React from "react";
 
@@ -7,26 +8,33 @@ const Featured = () => {
       {/* Wrapper */}
       <div className="w-max flex">
         {/* single item */}
-        <div className="w-screen h-[60vh] flex flex-col items-center justify-around">
-          {/* img */}
-          <div className="relative flex-1 w-full">
-            <Image
-              className="object-contain"
-              src="/temporary/p1.png"
-              alt=""
-              fill
-            ></Image>
+        {featuredProducts.map((product) => (
+          <div
+            key={product.id}
+            className="w-screen h-[60vh] flex flex-col items-center justify-around"
+          >
+            {/* img */}
+            {product.img && (
+              <div className="relative flex-1 w-full">
+                <Image
+                  className="object-contain"
+                  src={product.img}
+                  alt=""
+                  fill
+                ></Image>
+              </div>
+            )}
+            {/* text */}
+            <div className="flex-1 flex flex-col items-center gap-2 mt-2">
+              <h1 className="font-bold text-xl uppercase">{product.title}</h1>
+              <p className="text-center"> {product.desc} </p>
+              <span className="font-semibold text-xl"> ${product.price} </span>
+              <button className="bg-red-500 p-2 rounded-md text-white ">
+                Add to cart
+              </button>
+            </div>
           </div>
-          {/* text */}
-          <div className="flex-1 flex flex-col items-center gap-2 mt-2">
-            <h1 className="font-bold text-xl uppercase">title</h1>
-            <p>des</p>
-            <span>124</span>
-            <button className="bg-red-500 p-2 rounded-md text-white ">
-              Add to cart
-            </button>
-          </div>
-        </div>
+        ))}
       </div>
     </div>
   );
